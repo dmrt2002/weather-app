89063c59acbbccf5a8c2c9578da74c6b421fa600
dmrt2002 <72452238+dmrt2002@users.noreply.github.com>
2021-06-02 18:52:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const searchbox = document.querySelector(".search-box");
 const request = document.querySelector(".request");
+var xhttp = new XMLHttpRequest();
+
 function Setquery() {
     getresults(searchbox.value);
 }
 
 function getresults(query){
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=886705b4c1182eb1c69f28eb8c520e20`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=886705b4c1182eb1c69f28eb8c520e20`;
     fetch(url)
-    .then(weather => {
+    .then(function(weather) {
+        if (!weather.ok) {
+            throw Error(weather.statusText);
+        }
         return weather.json();
-    }).then(displayResults);
-}
-
-function displayResults(weather) {
-    request.innerHTML = "";
-    console.log(weather);
-
+    }).then(function(weather) {
+        
     let city = document.querySelector(".location .city");
     city.innerText = `${weather.name} , ${weather.sys.country}`;
 
@@ -27,15 +27,27 @@ function displayResults(weather) {
     temp.innerHTML = `${Math.round(weather.main.temp - 273.15)}<span> °C</span>`;
 
     let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
+    weather_el.innerHTML = weather.weather[0].description;
+    weather_el.innerHTML = weather_el.innerHTML.toUpperCase();
 
     let hilow = document.querySelector('.hi-low');
-   hilow.innerText = `${Math.round(weather.main.temp_min - 273.15)} °C / ${Math.round(weather.main.temp_max - 273.15)}`;
+    hilow.innerText = `${Math.round(weather.main.temp_min - 273.15)} °C / ${Math.round(weather.main.temp_max - 273.15)}`;
+    
+    }).catch(function(error) {
+        request.innerHTML = "Enter a Valid City Name"
+    });
+      
+}
 
+function displayResults(weather) {
+    request.innerHTML = "";
+    console.log(weather);
+
+    
 }
 
 function dateBuilder (d) {
-     let months = ["January","February", "March", "April" , "May" , "June" , "July","August","September", "October" , "november" , "december"];
+    let months = ["January","February", "March", "April" , "May" , "June" , "July","August","September", "October" , "november" , "december"];
     let days = ["Sunday","Monday", "Tuesday" , "Wednesday" , "Thursday","Friday","Saturday"];
 
     let day = days[d.getDay()];
